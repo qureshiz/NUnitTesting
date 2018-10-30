08246a139ee4cb97212811d41e34ff547e482557
qureshiz <41581171+qureshiz@users.noreply.github.com>
2018-10-30 09:05:28 +0000

    Add project files.

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+ [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+            var result = sut.Add(1, 3);
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+            var result = sut.Multiply(2, 3);
+            Assert.That(result,Is.EqualTo(6));
+
+        }
+
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Multiply(int a, int b) => a + b;
+    }
+}
